5a10cc7b22ea72212147000b3cd21cf833b3979c
lypluo <yluo@bgc-jena.mpg.de>
2022-12-01 17:57:22 +0100

    extract the VIs

--- data-raw/Extracting_camera_VIs.R
+++ data-raw/Extracting_camera_VIs.R
@@ -1,5 +1,6 @@
 ###################################################
 #Aim:Extracting the VIs from the Camera
+#processed the PhenoCam data that Lorenze sent me (data range from Mar,2021-Oct,2022)
 ###################################################
 Sys.setenv(tz="UTC")
 library(phenopix)
@@ -10,49 +11,69 @@ library(tidyverse)
 folder.path<-"./data-raw/"
 # structureFolder(folder.path)  ##create the folders for analysis
 
-#-----set the path----
-## set path of the reference image where to draw ROIs. Only one jpeg image is allowed in this folder.
-path.image.ref <- paste(folder.path,'REF/',sep='')  #here is the path for reference image
-## set path where to store the ROI coordinates
-path.roi <- paste(folder.path,'ROI/',sep='')
-## define path with all images to be processed
-# img.path <- paste(folder.path,'IMG/',sep='')
-img.path <- "D:/data/Test_data/PhenoCam_GCC/Lorenz_Walthert/Test_greenness/change_name/" #change the path
-## define in which folder VI data will be stored
-vi.path <- paste(folder.path,'VI/',sep='')
+years<-c(2021,2022)
 
+for (i in 1:length(years)) {
 
-#---------------------
-#(1)select the tree ROI:Tree1,Tree2
-#---------------------
+  #-----set the path----
+  ## set path of the reference image where to draw ROIs. Only one jpeg image is allowed in this folder.
+  path.image.ref <- paste(folder.path,'REF/',sep='')  #here is the path for reference image
+  ## set path where to store the ROI coordinates
+  path.roi <- paste(folder.path,'ROI/',sep='')
+  ## define path with all images to be processed
+  img.path <- paste0("D:/data/WSL_PhenoCam/Lorenz_Walthert/images_2021_2022/",years[i],"/") #change the path
+  ## define in which folder VI data will be stored
+  vi.path <- paste0(folder.path,'VI/',years[i],"/")
 
-roi.names <- c('tree1', 'tree2')
-nroi=length(roi.names)
-drawROI<-FALSE
+  #months:
+  months<-list.files(img.path)
+  #---------------------
+  #(1)select the tree ROI:Tree1,Tree2-->this has done!
+  #---------------------
 
-if (drawROI == TRUE){
+  roi.names <- c('tree1', 'tree2')
+  nroi=length(roi.names)
+  # drawROI<-FALSE
+  #
+  # if (drawROI == TRUE){
+  #
+  #   DrawMULTIROI(path.image.ref, path.roi, nroi=nroi,roi.names,file.type = ".JPG")
+  #
+  # }
 
-  DrawMULTIROI(path.image.ref, path.roi, nroi=nroi,roi.names,file.type = ".JPG")
+  #---------------------
+  #(2)vegetation indices(VIs) extraction
+  #---------------------
+  for (j in 5:length(months)) {
+
+  extractVIs(img.path = paste0(img.path,months[j],"/"),roi.path = path.roi,
+               vi.path = vi.path,roi.name = roi.names,
+               #set the begin date to save the data with different names
+               begin = paste0(years[i],"-",months[j],"-","01"),
+               plot=TRUE,
+               date.code="yyyymmddHHMMSS",ncores = "all",
+               file.type = ".JPG")
+  #
+    print(j)
+  }
 
 }
 
-#---------------------
-#(2)vegetation indices(VIs) extraction
-#---------------------
 
-extractVIs(img.path = img.path,roi.path = path.roi,
-                    vi.path = vi.path,roi.name = roi.names,
-                    plot=TRUE,
-                    date.code="yyyy-mm-dd-HHMM",ncores = 16,
-                    file.type = ".JPG")
-
-## notes: the object VI.site is actually a list of length == nrois. Each object of the list is names with
-## roi.names. Vi.data is saved as RData in vi.path, the name of the dataframe is VI.data
-load(paste(vi.path,'VI.data.Rdata',sep=''))
 
 #---------------------------------
 #(3)filter the data and the needed data
 #---------------------------------
+
+
+## notes: the object VI.site is actually a list of length == nrois. Each object of the list is names with
+## roi.names. Vi.data is saved as RData in vi.path, the name of the dataframe is VI.data
+load(paste(vi.path,'2022-10-01_2022-10-31_VI.data.Rdata',sep=''))
+
+
+
+
+
 filtered.VI_tree1<-autoFilter(VI.data$tree1,dn=c("r.av","g.av","b.av"),raw.dn = TRUE,filter = "max",plot = T)
 filtered.VI_tree2<-autoFilter(VI.data$tree2,dn=c("r.av","g.av","b.av"),raw.dn = TRUE,filter = "max",plot = T)
 
